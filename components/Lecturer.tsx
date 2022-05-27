41245e333e4efe3dc90152d40e74db06aaf9989f
Barak Haviv <barak.noam.haviv@gmail.com>
2022-05-27 17:50:41 +0300

    Add animation with react transition

--- components/Lecturer.tsx
+++ components/Lecturer.tsx
@@ -7,36 +7,39 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Lecturer } from '../types';
+import Slide from '@mui/material/Slide';
 
 export default function LecturerView({ lecturer }: { lecturer: Lecturer }) {
   return (
-    <Card sx={{ maxWidth: 345, justifyContent: 'center' }}>
-      <CardMedia
-        component="img"
-        style={{ height: 300 }}
-        image={lecturer.imageUrl}
-        alt={lecturer.name}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {lecturer.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {lecturer.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            flexWrap: 'wrap',
-            color: '#6363d5',
-          }}
-        >
-          <MailIcon sx={{ m: 1 }} /> {lecturer.email}
-        </Box>
-      </CardActions>
-    </Card>
+    <Slide direction="up" in={true} mountOnEnter unmountOnExit>
+      <Card sx={{ maxWidth: 345, justifyContent: 'center' }}>
+        <CardMedia
+          component="img"
+          style={{ height: 300 }}
+          image={lecturer.imageUrl}
+          alt={lecturer.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {lecturer.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {lecturer.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+              color: '#6363d5',
+            }}
+          >
+            <MailIcon sx={{ m: 1 }} /> {lecturer.email}
+          </Box>
+        </CardActions>
+      </Card>
+    </Slide>
   );
 }
